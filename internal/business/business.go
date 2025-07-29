00c14b89438560f79c25b9589bc2bc6ab1ff7b03
bimapw4 <bimapw4@gmail.com>
2025-07-29 12:50:51 +0700

    [cars] business - add business crud  cars

--- internal/business/business.go
+++ internal/business/business.go
@@ -1,12 +1,16 @@
 package business
 
 import (
+	"car-rent/internal/business/cars"
 	"car-rent/internal/repositories"
 )
 
 type Business struct {
+	Cars cars.Cars
 }
 
 func NewBusiness(repo *repositories.Repository) Business {
-	return Business{}
+	return Business{
+		Cars: cars.NewBusiness(repo),
+	}
 }
---
+++ internal/business/cars/cars.go
@@ -0,0 +1,144 @@
+package cars
+
+import (
+	"car-rent/internal/entity"
+	"car-rent/internal/presentations"
+	"car-rent/internal/repositories"
+	"car-rent/pkg/meta"
+	"time"
+
+	"context"
+)
+
+type Cars interface {
+	Create(ctx context.Context, payload entity.Cars) (*presentations.Cars, error)
+	List(ctx context.Context, m *meta.Params) ([]presentations.Cars, error)
+	Detail(ctx context.Context, carsID int) (*presentations.Cars, error)
+	Update(ctx context.Context, payload entity.Cars, carsID int) (*presentations.Cars, error)
+	Delete(ctx context.Context, carsID int) error
+	Activate(ctx context.Context, carsID int) error
+	Deactivate(ctx context.Context, carsID int) error
+}
+
+type business struct {
+	repo *repositories.Repository
+}
+
+func NewBusiness(repo *repositories.Repository) Cars {
+	return &business{
+		repo: repo,
+	}
+}
+
+func (b *business) Create(ctx context.Context, payload entity.Cars) (*presentations.Cars, error) {
+
+	err := b.repo.Cars.Create(ctx, presentations.Cars{
+		CarName:   payload.CarsName,
+		DayRate:   payload.DayRate,
+		MonthRate: payload.MonthRate,
+		Image:     payload.Image,
+		IsActive:  true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := b.repo.Cars.Latest(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (b *business) List(ctx context.Context, m *meta.Params) ([]presentations.Cars, error) {
+
+	res, err := b.repo.Cars.List(ctx, m)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (b *business) Detail(ctx context.Context, carsID int) (*presentations.Cars, error) {
+
+	res, err := b.repo.Cars.Detail(ctx, carsID)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (b *business) Update(ctx context.Context, payload entity.Cars, carsID int) (*presentations.Cars, error) {
+
+	cars, err := b.repo.Cars.Detail(ctx, carsID)
+	if err != nil {
+		return nil, err
+	}
+
+	data := presentations.Cars{
+		CarID:     carsID,
+		CarName:   payload.CarsName,
+		DayRate:   payload.DayRate,
+		MonthRate: payload.MonthRate,
+		Image:     payload.Image,
+		IsActive:  cars.IsActive,
+		CreatedAT: cars.CreatedAT,
+		UpdatedAt: time.Now().Local(),
+	}
+
+	err = b.repo.Cars.Update(ctx, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
+
+func (b *business) Delete(ctx context.Context, carsID int) error {
+	_, err := b.repo.Cars.Detail(ctx, carsID)
+	if err != nil {
+		return err
+	}
+
+	err = b.repo.Cars.Delete(ctx, carsID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *business) Activate(ctx context.Context, carsID int) error {
+	cars, err := b.repo.Cars.Detail(ctx, carsID)
+	if err != nil {
+		return err
+	}
+
+	if cars.IsActive {
+		return presentations.ErrCarsAlreadyActivate
+	}
+
+	err = b.repo.Cars.UpdateIsActive(ctx, carsID, true)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *business) Deactivate(ctx context.Context, carsID int) error {
+	cars, err := b.repo.Cars.Detail(ctx, carsID)
+	if err != nil {
+		return err
+	}
+
+	if !cars.IsActive {
+		return presentations.ErrCarsAlreadyDeactivate
+	}
+
+	err = b.repo.Cars.UpdateIsActive(ctx, carsID, false)
+	if err != nil {
+		return err
+	}
+	return nil
+}
