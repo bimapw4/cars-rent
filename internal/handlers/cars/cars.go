e4b4952fedbefaf48f390326c270c0065f75ecad
bimapw4 <bimapw4@gmail.com>
2025-07-29 12:51:08 +0700

    [cars] handler - add handler crud cars

---
+++ internal/handlers/cars/cars.go
@@ -0,0 +1,321 @@
+package cars
+
+import (
+	"car-rent/internal/business"
+	"car-rent/internal/common"
+	"car-rent/internal/entity"
+	"car-rent/internal/presentations"
+	"car-rent/internal/response"
+	"car-rent/pkg/meta"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Handler interface {
+	Create(c *fiber.Ctx) error
+	Update(c *fiber.Ctx) error
+	List(c *fiber.Ctx) error
+	Detail(c *fiber.Ctx) error
+	Delete(c *fiber.Ctx) error
+	Activate(c *fiber.Ctx) error
+	Deactivate(c *fiber.Ctx) error
+}
+
+type handler struct {
+	business business.Business
+}
+
+func NewHandler(business business.Business) Handler {
+	return &handler{
+		business: business,
+	}
+}
+
+func (h *handler) Create(c *fiber.Ctx) error {
+	var (
+		Entity = "CreateCars"
+	)
+	errAvail := common.DefaultAvailableErrors()
+	custErr := errAvail.CustomeError(common.AvailableErrors{
+		{
+			Code:    http.StatusNotFound,
+			Err:     presentations.ErrCarsNotExist,
+			Message: presentations.ErrCarsNotExist.Error(),
+		},
+		{
+			Code:    http.StatusConflict,
+			Err:     presentations.ErrCarsAlreadyExist,
+			Message: presentations.ErrCarsAlreadyExist.Error(),
+		},
+	})
+
+	var payload entity.Cars
+	if err := c.BodyParser(&payload); err != nil {
+		return response.NewResponse(Entity).
+			Errors("err parse body payload", err.Error()).
+			JSON(c, http.StatusBadRequest)
+	}
+
+	res, err := h.business.Cars.Create(c.UserContext(), payload)
+	if err != nil {
+		fmt.Println("err == ", err)
+		errs := custErr.GetError(err)
+		return response.NewResponse(Entity).
+			Errors("err create cars", errs.Message).
+			JSON(c, errs.Code)
+	}
+
+	return response.NewResponse(Entity).
+		Success("Successfully Create Cars", res).
+		JSON(c, http.StatusCreated)
+}
+
+func (h *handler) Update(c *fiber.Ctx) error {
+	var (
+		Entity = "UpdateCars"
+	)
+	errAvail := common.DefaultAvailableErrors()
+	custErr := errAvail.CustomeError(common.AvailableErrors{
+		{
+			Code:    http.StatusNotFound,
+			Err:     presentations.ErrCarsNotExist,
+			Message: presentations.ErrCarsNotExist.Error(),
+		},
+		{
+			Code:    http.StatusConflict,
+			Err:     presentations.ErrCarsAlreadyExist,
+			Message: presentations.ErrCarsAlreadyExist.Error(),
+		},
+	})
+
+	var payload entity.Cars
+	if err := c.BodyParser(&payload); err != nil {
+		return response.NewResponse(Entity).
+			Errors("err parse body payload", err.Error()).
+			JSON(c, http.StatusBadRequest)
+	}
+
+	carsID := c.Params("cars_id")
+	intCars, err := strconv.Atoi(carsID)
+	if err != nil {
+		return response.NewResponse(Entity).
+			Errors("err parse params", err.Error()).
+			JSON(c, http.StatusBadRequest)
+	}
+
+	res, err := h.business.Cars.Update(c.UserContext(), payload, intCars)
+	if err != nil {
+		fmt.Println("err =", err)
+		errs := custErr.GetError(err)
+		return response.NewResponse(Entity).
+			Errors("err update cars", errs.Message).
+			JSON(c, errs.Code)
+	}
+
+	return response.NewResponse(Entity).
+		Success("Successfully Update Cars", res).
+		JSON(c, http.StatusAccepted)
+}
+
+func (h *handler) List(c *fiber.Ctx) error {
+	var (
+		Entity = "ListCars"
+	)
+	errAvail := common.DefaultAvailableErrors()
+	custErr := errAvail.CustomeError(common.AvailableErrors{
+		{
+			Code:    http.StatusNotFound,
+			Err:     presentations.ErrCarsNotExist,
+			Message: presentations.ErrCarsNotExist.Error(),
+		},
+		{
+			Code:    http.StatusConflict,
+			Err:     presentations.ErrCarsAlreadyExist,
+			Message: presentations.ErrCarsAlreadyExist.Error(),
+		},
+	})
+
+	q := c.Queries()
+	m := meta.NewParams(q)
+
+	res, err := h.business.Cars.List(c.UserContext(), &m)
+	if err != nil {
+		errs := custErr.GetError(err)
+		return response.NewResponse(Entity).
+			Errors("err parse body payload", errs.Message).
+			JSON(c, errs.Code)
+	}
+
+	return response.NewResponse(Entity).
+		SuccessWithMeta("Successfully List Cars", res, m).
+		JSON(c, http.StatusOK)
+}
+
+func (h *handler) Detail(c *fiber.Ctx) error {
+	var (
+		Entity = "DetailCars"
+	)
+	errAvail := common.DefaultAvailableErrors()
+	custErr := errAvail.CustomeError(common.AvailableErrors{
+		{
+			Code:    http.StatusNotFound,
+			Err:     presentations.ErrCarsNotExist,
+			Message: presentations.ErrCarsNotExist.Error(),
+		},
+		{
+			Code:    http.StatusConflict,
+			Err:     presentations.ErrCarsAlreadyExist,
+			Message: presentations.ErrCarsAlreadyExist.Error(),
+		},
+	})
+
+	carsID := c.Params("cars_id")
+	intCars, err := strconv.Atoi(carsID)
+	if err != nil {
+		return response.NewResponse(Entity).
+			Errors("err parse params", err.Error()).
+			JSON(c, http.StatusBadRequest)
+	}
+
+	res, err := h.business.Cars.Detail(c.UserContext(), intCars)
+	if err != nil {
+		errs := custErr.GetError(err)
+		return response.NewResponse(Entity).
+			Errors("err get detail cars", errs.Message).
+			JSON(c, errs.Code)
+	}
+
+	return response.NewResponse(Entity).
+		Success("Successfully Detail Cars", res).
+		JSON(c, http.StatusOK)
+}
+
+func (h *handler) Delete(c *fiber.Ctx) error {
+	var (
+		Entity = "DeleteCars"
+	)
+	errAvail := common.DefaultAvailableErrors()
+	custErr := errAvail.CustomeError(common.AvailableErrors{
+		{
+			Code:    http.StatusNotFound,
+			Err:     presentations.ErrCarsNotExist,
+			Message: presentations.ErrCarsNotExist.Error(),
+		},
+		{
+			Code:    http.StatusConflict,
+			Err:     presentations.ErrCarsAlreadyExist,
+			Message: presentations.ErrCarsAlreadyExist.Error(),
+		},
+	})
+
+	carsID := c.Params("cars_id")
+	intCars, err := strconv.Atoi(carsID)
+	if err != nil {
+		return response.NewResponse(Entity).
+			Errors("err parse params", err.Error()).
+			JSON(c, http.StatusBadRequest)
+	}
+
+	err = h.business.Cars.Delete(c.UserContext(), intCars)
+	if err != nil {
+		errs := custErr.GetError(err)
+		return response.NewResponse(Entity).
+			Errors("err delete cars", errs.Message).
+			JSON(c, errs.Code)
+	}
+
+	return response.NewResponse(Entity).
+		Success("Successfully Delete Cars", nil).
+		JSON(c, http.StatusOK)
+}
+
+func (h *handler) Activate(c *fiber.Ctx) error {
+	var (
+		Entity = "ActivateCars"
+	)
+	errAvail := common.DefaultAvailableErrors()
+	custErr := errAvail.CustomeError(common.AvailableErrors{
+		{
+			Code:    http.StatusNotFound,
+			Err:     presentations.ErrCarsNotExist,
+			Message: presentations.ErrCarsNotExist.Error(),
+		},
+		{
+			Code:    http.StatusConflict,
+			Err:     presentations.ErrCarsAlreadyExist,
+			Message: presentations.ErrCarsAlreadyExist.Error(),
+		},
+		{
+			Code:    http.StatusConflict,
+			Err:     presentations.ErrCarsAlreadyActivate,
+			Message: presentations.ErrCarsAlreadyActivate.Error(),
+		},
+	})
+
+	carsID := c.Params("cars_id")
+	intCars, err := strconv.Atoi(carsID)
+	if err != nil {
+		return response.NewResponse(Entity).
+			Errors("err parse params", err.Error()).
+			JSON(c, http.StatusBadRequest)
+	}
+
+	err = h.business.Cars.Activate(c.UserContext(), intCars)
+	if err != nil {
+		errs := custErr.GetError(err)
+		return response.NewResponse(Entity).
+			Errors("err activate cars", errs.Message).
+			JSON(c, errs.Code)
+	}
+
+	return response.NewResponse(Entity).
+		Success("Successfully Activate Cars", nil).
+		JSON(c, http.StatusOK)
+}
+
+func (h *handler) Deactivate(c *fiber.Ctx) error {
+	var (
+		Entity = "DeactivateCars"
+	)
+	errAvail := common.DefaultAvailableErrors()
+	custErr := errAvail.CustomeError(common.AvailableErrors{
+		{
+			Code:    http.StatusNotFound,
+			Err:     presentations.ErrCarsNotExist,
+			Message: presentations.ErrCarsNotExist.Error(),
+		},
+		{
+			Code:    http.StatusConflict,
+			Err:     presentations.ErrCarsAlreadyExist,
+			Message: presentations.ErrCarsAlreadyExist.Error(),
+		},
+		{
+			Code:    http.StatusConflict,
+			Err:     presentations.ErrCarsAlreadyDeactivate,
+			Message: presentations.ErrCarsAlreadyDeactivate.Error(),
+		},
+	})
+
+	carsID := c.Params("cars_id")
+	intCars, err := strconv.Atoi(carsID)
+	if err != nil {
+		return response.NewResponse(Entity).
+			Errors("err parse params", err.Error()).
+			JSON(c, http.StatusBadRequest)
+	}
+
+	err = h.business.Cars.Deactivate(c.UserContext(), intCars)
+	if err != nil {
+		errs := custErr.GetError(err)
+		return response.NewResponse(Entity).
+			Errors("err deactivate cars", errs.Message).
+			JSON(c, errs.Code)
+	}
+
+	return response.NewResponse(Entity).
+		Success("Successfully Deactivate Cars", nil).
+		JSON(c, http.StatusOK)
+}
--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -1,10 +1,16 @@
 package handlers
 
-import "car-rent/internal/business"
+import (
+	"car-rent/internal/business"
+	"car-rent/internal/handlers/cars"
+)
 
 type Handlers struct {
+	Cars cars.Handler
 }
 
 func NewHandler(business business.Business) Handlers {
-	return Handlers{}
+	return Handlers{
+		Cars: cars.NewHandler(business),
+	}
 }
